b2917b321a5d0de03dcc5ddaf643b81ed12f4fc9
Ashish Rai <100395176+ashishrair500@users.noreply.github.com>
2023-09-28 22:50:08 +0530

    Time: 2 ms (89.38%), Space: 45.4 MB (70.47%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1299-replace-elements-with-greatest-element-on-right-side/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1299-replace-elements-with-greatest-element-on-right-side.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1299-replace-elements-with-greatest-element-on-right-side/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1299-replace-elements-with-greatest-element-on-right-side.java
@@ -2,20 +2,42 @@ class Solution {
     public int[] replaceElements(int[] arr) {
        
         //brute force approach
-        int max=Integer.MIN_VALUE;
+      /*  int max=Integer.MIN_VALUE;
         int n= arr.length;
          int ans []= new int[n];
         for(int i=0; i<n-1; i++){
             max=Integer.MIN_VALUE;
             for(int j=i+1; j<arr.length; j++){
                 if(max<arr[j]) max=arr[j];
-                         
-                
             }
             ans[i]=max;
             
         }
         ans[n-1]=-1;
+        return ans;
+        
+        */
+        
+        
+        //better approach
+        
+        int max=-1;
+        int carryOver=Integer.MIN_VALUE;
+        int ans[]= new int[arr.length];
+        
+       for(int i=arr.length-1; i>=0;i--){
+           if(arr[i]>max){
+               carryOver=arr[i];
+               ans[i]=max;
+               max=carryOver;
+               
+           }else{
+               ans[i]=max;
+               
+           }
+           
+           
+       } 
         return ans;
     }
 }
