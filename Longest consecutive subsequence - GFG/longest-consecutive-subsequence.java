30e11d2398d6590b5ce4144c5d0fcf19a0381f16
Ashish Rai <100395176+ashishrair500@users.noreply.github.com>
2023-09-29 16:35:29 +0530

    Added solution - LeetHub

---
+++ Longest consecutive subsequence - GFG/longest-consecutive-subsequence.java
@@ -0,0 +1,99 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.math.*;
+import java.util.*;
+import java.io.*;
+
+class Driverclass
+{
+    // Driver Code
+    static class FastReader{ 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader(){ 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next(){ 
+            while (st == null || !st.hasMoreElements()){ 
+                try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        String nextLine(){ 
+            String str = ""; 
+            try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+            return str; 
+        } 
+
+        Integer nextInt(){
+            return Integer.parseInt(next());
+        }
+    }
+    
+	public static void main(String args[])
+	{
+		FastReader sc = new FastReader();
+		PrintWriter out = new PrintWriter(System.out);
+		int t = sc.nextInt();
+		
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int a[] = new int[n];
+			
+			for(int i=0; i<n; i++)
+				a[i] = sc.nextInt();
+		    out.println(new Solution().findLongestConseqSubseq(a, n));
+		    t--;
+		}
+		out.close();
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{   
+    // arr[] : the input array
+    // N : size of the array arr[]
+    
+    //Function to return length of longest subsequence of consecutive integers.
+	static int findLongestConseqSubseq(int nums[], int N)
+	{
+	   // add your code here
+	    if(nums.length==0) return 0;
+        Set<Integer> ans = new TreeSet<>();
+        
+        int min=Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            ans.add(nums[i]);
+           min=Math.min(min,nums[i]);
+        }
+
+        int count = 0;
+        int check =min ;
+        int anss = Integer.MIN_VALUE;
+
+       for (Integer in : ans) {
+
+          if (in == check){
+                count++;
+                anss = Math.max(anss, count);
+                check++;
+            } else {
+                check = in;
+                count = 1;
+                check++;
+
+            }
+         }
+
+        return anss;
+	}
+}
