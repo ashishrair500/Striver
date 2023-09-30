641e080cf46a23c25716cbcdb055a0d69d3ebd94
Ashish Rai <100395176+ashishrair500@users.noreply.github.com>
2023-09-30 17:36:29 +0530

    Time: 1 ms (90.11%), Space: 44.6 MB (53.08%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,8 +2,9 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         
         //brute force approach with O(n) time complexity
-        int n=matrix.length;
-    int m=matrix[0].length;
+        
+        /*
+       
         
         int row[]=new int[n];
         int col[]=new int[m];
@@ -33,7 +34,43 @@ class Solution {
             }
         }   
         
+        */
         
+        //optimal approach taken O(n^2)  but no extra space
+         int n=matrix.length;
+        int m=matrix[0].length;
+        int col0=-1;
+        
+         for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    if(j!=0){
+                        matrix[0][j]=0;
+                    }else{
+                   col0=0;     
+                    }
+                }
+            }
+         }
+        
+        
+         for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                
+                if(matrix[i][j]!=0){
+                   if(matrix[0][j]==0 || matrix[i][0]==0){
+                       matrix[i][j]=0;
+                   }
+                    
+                }
+            }
+         }
+        
+        
+        if(matrix[0][0]==0) for(int j=0; j<m; j++) matrix[0][j]=0;
+        
+        if(col0==0) for(int i=0; i<n; i++) matrix[i][0]=0;
         
     }
         
