2f3bc09f7e7834728281d4237be3576cc335153c
Ashish Rai <100395176+ashishrair500@users.noreply.github.com>
2023-10-25 22:38:50 +0530

    Time: 18 ms (18.09%), Space: 43.3 MB (63.91%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+         if(s.length()!=t.length()) return false;
+        
+        HashMap<Character,Integer>hm = new HashMap<>();
+        HashMap<Character,Integer>hm2 = new HashMap<>();
+
+        for(int i=0; i<s.length(); i++ ){
+            
+            if(hm.containsKey(s.charAt(i))){
+                hm.put(s.charAt(i),hm.get(s.charAt(i))+1);
+            }
+            else{ hm.put(s.charAt(i),1);
+                }
+        }
+        
+        for(int i=0; i<t.length(); i++ ){
+            if(hm2.containsKey(t.charAt(i))){
+                hm2.put(t.charAt(i),hm2.get(t.charAt(i))+1);
+            }
+            else {
+                hm2.put(t.charAt(i),1);
+            }
+            
+        }
+        
+        for(char k:hm.keySet()){
+            if(!hm.get(k).equals(hm2.get(k))) return false;
+            
+        }
+        return true;
+    
+    }
+}
