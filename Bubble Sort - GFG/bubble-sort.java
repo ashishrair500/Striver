58d83c3e94e95737acb9f02805862323847eaf61
Ashish Rai <100395176+ashishrair500@users.noreply.github.com>
2024-01-01 22:22:28 +0530

    Added solution - LeetHub

--- Bubble Sort - GFG/bubble-sort.java
+++ Bubble Sort - GFG/bubble-sort.java
@@ -15,15 +15,16 @@ class Solution
     {
         //code here
         //side vale element ko thikane lagana hai
-        for(int i=0; i<n-1; i++){
-            for(int j=0; j<n-i-1; j++){
-                if(arr[j]>arr[j+1]){
+        for(int i=0; i<n; i++){
+            
+        for(int j=0; j<n-1-i; j++){
+            if(arr[j]>arr[j+1]){
                 int temp=arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=temp;
-                    
-                }
             }
+            
+          }
         }
     }
 }
