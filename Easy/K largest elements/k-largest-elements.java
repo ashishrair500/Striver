5c1008fd35a7a4ad391e365747810b898f4a0474
Ashish Rai <100395176+ashishrair500@users.noreply.github.com>
2024-03-12 14:12:52 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/K largest elements/k-largest-elements.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t > 0)
+        {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            
+            int arr[] = new int[n];
+            for(int i = 0; i<n; ++i)
+                arr[i] = sc.nextInt();
+            Solution ob = new Solution();
+            ArrayList<Integer> list = ob.kLargest(arr, n, k);
+            for(int i = 0; i<list.size(); i++)
+                System.out.print(list.get(i) + " ");
+            System.out.println();
+            t--;
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to return k largest elements from an array.
+    public static ArrayList<Integer> kLargest(int arr[], int n, int k)
+    {
+        ArrayList<Integer> a= new ArrayList<>();
+        Arrays.sort(arr);
+      //  Arrays.reverse(arr);
+        for(int i=n-k; i<=n-1; i++){
+            a.add(arr[i]);
+        }
+        Collections.reverse(a);
+        return a;
+        // code here 
+    }
+}
