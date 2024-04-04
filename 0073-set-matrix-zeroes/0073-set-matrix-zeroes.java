80f1b5150ba1170ab2e9fc32a96a5cf518990ccf
Ashish Rai <100395176+ashishrair500@users.noreply.github.com>
2024-04-04 16:52:25 +0530

    Update 0073-set-matrix-zeroes.java

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -69,7 +69,6 @@ class Solution {
         
         
         if(matrix[0][0]==0) for(int j=0; j<m; j++) matrix[0][j]=0;
-        
         if(col0==0) for(int i=0; i<n; i++) matrix[i][0]=0;
         
     }
