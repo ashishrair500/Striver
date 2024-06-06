649e2847684fe46d690ee6c5bd73069dfbd0b8d3
Ashish Rai <100395176+ashishrair500@users.noreply.github.com>
2024-06-06 21:48:07 +0530

    Updated Solution - GfG to GitHub

--- Medium/Sort a stack/sort-a-stack.java
+++ Medium/Sort a stack/sort-a-stack.java
@@ -1,50 +1,52 @@
 //{ Driver Code Starts
+import java.util.*;
 import java.util.Scanner;
 import java.util.Stack;
-class SortedStack{
-	public static void main(String[] args){
-		Scanner sc=new Scanner(System.in);
-		int t=sc.nextInt();
-		while(t-->0){
-			Stack<Integer> s=new Stack<>();
-			int n=sc.nextInt();
-			while(n-->0)
-			s.push(sc.nextInt());
-			GfG g=new GfG();
-			Stack<Integer> a=g.sort(s);
-			while(!a.empty()){
-				System.out.print(a.peek()+" ");
-				a.pop();
-			}
-			System.out.println();
-		}
-	}
+
+class SortedStack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            Stack<Integer> s = new Stack<>();
+            int n = sc.nextInt();
+            while (n-- > 0) s.push(sc.nextInt());
+            GfG g = new GfG();
+            Stack<Integer> a = g.sort(s);
+            while (!a.empty()) {
+                System.out.print(a.peek() + " ");
+                a.pop();
+            }
+            System.out.println();
+        }
+    }
 }
 // } Driver Code Ends
 
 
 /*Complete the function below*/
-class GfG{
-    
-   
-	public Stack<Integer> sort(Stack<Integer> s)
-	{
-		//add code here.
-	if(s.isEmpty()) return s;
-		int ele=s.pop();
-		sort(s);
-		insert(ele,s);
-		return s;
-	}
-	
-	public void insert(int ele, Stack<Integer>st){
-	    if(st.isEmpty() || st.peek()<ele){
-	    st.push(ele);
-	   return;
-	        
-	    }    
-	    int temp=st.pop();
-	    insert(ele,st);
-	    st.push(temp);
-	}
+class GfG {
+    static public void popq(int a ,Stack<Integer>st)
+    {
+        if(st.isEmpty() || st.peek()<a){
+            st.push(a);
+            return;
+        }
+    int z=st.pop();
+    popq(a,st);
+    st.push(z);
+    }    
+    public Stack<Integer> sort(Stack<Integer> s) {
+        // add code here.
+        Stack<Integer> st =new Stack<>();
+        st.push(s.pop());
+        
+        while(!s.isEmpty()){
+            int a=s.pop();
+            if(st.peek()>a){
+                popq(a,st);
+            }else st.push(a);
+        }
+        return st;
+    }
 }
